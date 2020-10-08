6dafa8e3612ac34155a02f3390b94590b2a41620
@willhead <60652390+will-head@users.noreply.github.com>
2020-10-08 16:14:16 +0100

    Refactors step definitions

--- SpecFlow-Mac-Example/TestSteps.cs
+++ SpecFlow-Mac-Example/TestSteps.cs
@@ -1,66 +1,39 @@
 ﻿using System;
 using Example;
 using FluentAssertions;
-
 using TechTalk.SpecFlow;
 
 namespace SpecFlowMacExample
 {
     [Binding]
-    public sealed class CalculatorStepDefinitions
+    public class CalculatorStepDefinitions
     {
 
-        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
-
-        private Calculator calculator = new Calculator();
-        private int result;
-
-
-        private readonly ScenarioContext _scenarioContext;
-
-        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
-        {
-            _scenarioContext = scenarioContext;
-        }
+        private readonly Calculator _calculator = new Calculator();
+        private int _result;
 
         [Given("the first number is (.*)")]
         public void GivenTheFirstNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
-
-            calculator.FirstNumber = number;
+            _calculator.FirstNumber = number;
         }
 
         [Given("the second number is (.*)")]
         public void GivenTheSecondNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
-
-            calculator.SecondNumber = number;
+            _calculator.SecondNumber = number;
         }
 
         [When("the two numbers are added")]
         public void WhenTheTwoNumbersAreAdded()
         {
-            //TODO: implement act (action) logic
-
-            result = calculator.Add();
+            _result = _calculator.Add();
         }
 
         [Then("the result should be (.*)")]
         public void ThenTheResultShouldBe(int expectedResult)
         {
-            //TODO: implement assert (verification) logic
-
-            result.Should().Be(expectedResult);
+            _result.Should().Be(expectedResult);
         }
     }
 }
